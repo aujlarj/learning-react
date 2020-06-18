cc684c761692f53d63e1daf6004a5ab2c440749f
aujlarj <aujla.rj@hotmail.com>
2020-06-17 22:24:21 -0700

    bootstrap added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // state = {  }
+  state = {
+    count: 0,
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <span style={this.styles}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
