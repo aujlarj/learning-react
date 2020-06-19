a0a04b561a9eeb40f820fc828282d938a89c62a6
aujlarj <aujla.rj@hotmail.com>
2020-06-19 13:14:35 -0700

    movie table created

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Movies from "./components/movies";
 import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
