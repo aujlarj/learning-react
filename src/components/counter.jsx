679166358baf165e73da9da3f7b2731d2252765b
aujlarj <aujla.rj@hotmail.com>
2020-06-22 15:21:47 -0700

    reset button done! local state created

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,9 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
-
   styles = {
     fontSize: 15,
     fontStyle: "bold",
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  handleDelete = () => {
-    this.setState({ value: this.state.value - 1 });
   };
 
   render() {
@@ -24,16 +12,18 @@ class Counter extends Component {
 
     return (
       <div>
-        <h4>{this.props.id}</h4>
         <span style={this.styles} className={classes}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-dark" onClick={this.handleIncrement}>
+        <button
+          className="btn btn-dark"
+          onClick={() => this.props.onIncrement(this.props.counter)}
+        >
           Increment
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
         >
           Delete
         </button>
@@ -43,13 +33,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
 
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -16,15 +16,42 @@ class Counters extends Component {
     const newCounters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters: newCounters });
   };
+
+  handleIncrement = (counter) => {
+    // 3 dots i.e. '...' does cloning
+    const newCounters = [...this.state.counters];
+    const index = newCounters.indexOf(counter);
+    console.log(index);
+    newCounters[index] = { ...counter };
+    newCounters[index].value++;
+    this.setState({ counters: newCounters });
+
+    // this.setState({ value: this.state.counters.value - 1 });
+  };
+
+  handleReset = () => {
+    // would give us new array of counters
+    const resettedCounters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: resettedCounters });
+  };
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            counter={counter} // incapsulate id and value
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
           ></Counter>
         ))}
       </div>
