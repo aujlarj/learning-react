d302bdfe1f6c20d35c21bd06fab4cb5696a77207
aujlarj <aujla.rj@hotmail.com>
2020-06-22 17:01:33 -0700

    total counter added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import Movies from "./components/movies";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar></Navbar>
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 3 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    console.log("Event Handler called", counterId);
+    const newCounters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: newCounters });
+  };
+
+  handleIncrement = (counter) => {
+    // 3 dots i.e. '...' does cloning
+    const newCounters = [...this.state.counters];
+    const index = newCounters.indexOf(counter);
+    console.log(index);
+    newCounters[index] = { ...counter };
+    newCounters[index].value++;
+    this.setState({ counters: newCounters });
+
+    // this.setState({ value: this.state.counters.value - 1 });
+  };
+
+  handleReset = () => {
+    // would give us new array of counters
+    const resettedCounters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: resettedCounters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        ></Navbar>
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,56 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 3 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    console.log("Event Handler called", counterId);
-    const newCounters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: newCounters });
-  };
-
-  handleIncrement = (counter) => {
-    // 3 dots i.e. '...' does cloning
-    const newCounters = [...this.state.counters];
-    const index = newCounters.indexOf(counter);
-    console.log(index);
-    newCounters[index] = { ...counter };
-    newCounters[index].value++;
-    this.setState({ counters: newCounters });
-
-    // this.setState({ value: this.state.counters.value - 1 });
-  };
-
-  handleReset = () => {
-    // would give us new array of counters
-    const resettedCounters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters: resettedCounters });
-  };
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
             counter={counter} // incapsulate id and value
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
           ></Counter>
         ))}
       </div>
