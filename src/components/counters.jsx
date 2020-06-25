26efe2a3c86778160df1ac9afea4dc02a4b82068
aujlarj <aujla.rj@hotmail.com>
2020-06-25 13:21:58 -0700

    stateless functional component created

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,6 +3,8 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
     return (
       <div>
         <button
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill badge-secondary m-2">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Function(Sateless Functional Component) can be used here instead of Class
+// template shortcut: 'sfc'
+// Example 1:
+// const NavBar = (props) => {
+//     return (
+//         <nav class="navbar navbar-light bg-light">
+//           <a class="navbar-brand" href="#">
+//             Navbar
+//             <span className="badge badge-pill badge-secondary m-2">
+//               {props.totalCounters}
+//             </span>
+//           </a>
+//         </nav>
+//       );
+// }
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary m-2">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav class="navbar navbar-light bg-light">
+//         <a class="navbar-brand" href="#">
+//           Navbar
+//           <span className="badge badge-pill badge-secondary m-2">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
 
 export default NavBar;
